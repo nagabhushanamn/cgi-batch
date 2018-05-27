520aedddd17e2dd6de4ad664460cdb103f14e557
Nagabhushanam <nagabhushanamn@gmail.com>
2018-05-27 12:42:33 +0530

    todo service

--- 6-todo-appln/src/service/TodoService.js
+++ 6-todo-appln/src/service/TodoService.js
@@ -12,25 +12,41 @@ export default class TodoService {
         // or
         this.todos = this.todos.concat(newTodo);
     }
-    editTodo(id, newTitle) {
-
+    editTodo(id, title) {
+        this.todos = this.todos.map(todo => id === todo.id ? Object.assign({}, todo, { title }) : todo)
     }
     completeTodo(id) {
-
+        this.todos = this.todos.map(todo => id === todo.id ? Object.assign({}, todo, { completed: !todo.completed }) : todo)
     }
     deleteTodo(id) {
-
+        this.todos = this.todos.filter(todo => id !== todo.id)
     }
     completeAll() {
-
+        let isAllComplete = this.todos.every(todo => todo.completed);
+        this.todos = this.todos.map(todo => Object.assign({}, todo, { completed: !isAllComplete }))
     }
     clearCompleted() {
-
+        this.todos = this.todos.filter(todo => !todo.completed)
     }
     getTodos(filter) {
         switch (filter) {
             case 'ALL':
                 this.todos.forEach(todo => console.log(todo));
+                return;
+            case 'ACTIVE':
+                this.todos.forEach(todo => {
+                    if (!todo.completed) {
+                        console.log(todo)
+                    }
+                });
+                return;
+            case 'COMPLETED':
+                this.todos.forEach(todo => {
+                    if (todo.completed) {
+                        console.log(todo)
+                    }
+                });
+                return;
         }
     }
 }
